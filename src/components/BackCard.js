c8466c3ea6990a36022c753ee1a9fe857e4701ab
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2021-02-04 17:52:04 +0000

    initial commit

---
+++ src/components/BackCard.js
@@ -0,0 +1,12 @@
+import ProfileImage from "../assets/images/avatar.jpg";
+
+const BackCard = (props) => {
+  return (<div className="" onMouseEnter={props.handleEvent} onMouseLeave={props.handleEvent} onClick={props.handleEvent}>
+  
+  
+
+  </div>
+  );
+}
+
+export default BackCard;
---
+++ src/components/FrontCard.js
@@ -0,0 +1,12 @@
+import ProfileImage from "../assets/images/avatar.jpg";
+
+const FrontCard = (props) => {
+  return (
+    <div className="" onMouseEnter={props.handleEvent} onMouseLeave={props.handleEvent} onClick={props.handleEvent}>
+
+
+    </div>
+  );
+}
+
+export default FrontCard;
